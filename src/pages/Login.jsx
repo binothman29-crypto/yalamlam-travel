ce2b2a83f9108289bd3b07595d751b701dc2af92
binothman29 <bin.othman29@gmail.com>
2026-08-05 07:23:25 +0300

    fix: add Link import to Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { supabase } from '../supabaseClient';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const [email, setEmail] = useState('');
